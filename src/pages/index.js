28cec88a70dc2bbae5582fc2d7af0b30d0f7a45f
leejina <zlor26@jiran.com>
2023-02-28 18:32:56 +0900

    fix: 디렉토리 구조 작업

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+  구조작업
+    </>
+  )
+}
