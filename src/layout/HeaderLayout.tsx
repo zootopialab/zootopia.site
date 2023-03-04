f2683ec4276bd99c2b3a3fc9a50f9e4fd951ac76
leejina <zlor26@jiran.com>
2023-03-04 18:55:45 +0900

    fix: HeaderLayout 생성

---
+++ src/layout/HeaderLayout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../components/header/Header";
+
+const HeaderLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex flex-col h-screen bg-gray-100 dark:bg-slate-900">
+      {/* Top nav*/}
+      <Header />
+
+      <div className="flex flex-1 min-h-0 overflow-hidden">
+        {/* Main area */}
+        <main className="flex flex-col flex-1 min-w-0 px-10 pt-8 pb-32 overflow-y-auto border-gray-200">
+          <div className="flex-1 w-full mx-auto min-w-min">{children}</div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderLayout;
