4629f69dd80445f4a45178aef4d88523339bf1bf
leejina <zlor26@jiran.com>
2023-03-07 18:22:34 +0900

    fix: 네이버지도 map 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import "../styles/globals.css";
 import { JWTUI } from "../components/useDialog/useDialog";
 import { ReactElement, ReactNode } from "react";
 import { NextPage } from "next";
+import { RecoilRoot } from "recoil";
+import Script from "next/script";
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -11,8 +13,12 @@ export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-    <>
+    <RecoilRoot>
+      <Script
+        strategy="beforeInteractive"
+        src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=id9esd8xgw"
+      />
       <JWTUI>{getLayout(<Component {...pageProps} />)}</JWTUI>
-    </>
+    </RecoilRoot>
   );
 }
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HeaderLayout from "../../layout/HeaderLayout";
+import useMap from "../../hooks/useMap";
+import KindFood from "../../components/main/KindFood";
 
 const Main = () => {
+  //지도
+  useMap();
+
   return (
     <>
-      <p>ddd</p>
+      <div className="w-full h-[300px]" id="map"></div>
+
+      <div className="my-10">
+        <p className="text-sm font-bold text-gray-800">[판교 추천 List]</p>
+        <KindFood />
+      </div>
+
+      <div>
+        <p>쥬토 member들이 추천하는 맛집 List</p>
+      </div>
     </>
   );
 };
