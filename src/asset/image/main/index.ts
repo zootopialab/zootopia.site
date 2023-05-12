34e7dad3741f558bb880b470d88cce71a2654984
leejina <zlor26@jiran.com>
2023-05-12 22:20:11 +0900

    fix: login페이지 디자인 수정

--- src/asset/image/main/index.ts
+++ src/asset/image/main/index.ts
@@ -1,2 +1,1 @@
-export { default as photoFood } from "./photoFood.jpg";
 export { default as Together1 } from "./together1.jpeg";
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,195 +1,88 @@
+import React from "react";
 import { Button } from "@mui/material";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import React from "react";
-import { Logo } from "../asset/image";
-import VocieLogin from "../components/login/VocieLogin";
 import { useDialog } from "../components/useDialog/useDialog";
 import TextField from "../components/utils/Textfield";
 import Label from "../components/utils/Lable";
 import BaseInput from "../components/utils/Input";
-import { photoFood } from "../asset/image/main";
 
 const Login = () => {
   const { openSidebar } = useDialog();
   const router = useRouter();
 
-  const handleClick = () => {
-    openSidebar(<VocieLogin />);
-  };
-
   return (
     <>
-      <div className="flex h-screen">
-        <div className="flex flex-col justify-center flex-1 px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
-          <div className="w-full max-w-sm mx-auto lg:w-96">
+      <div className="flex items-center justify-center flex-1 h-screen min-h-full px-4 py-12 sm:px-6 lg:px-8">
+        <div className="w-full max-w-sm space-y-10">
+          <div>
+            <Image
+              className="rounded-full cursor-pointer ml-44"
+              src="https://tistory3.daumcdn.net/tistory/571103/attach/c87f780fad0443338143eda499b54d37"
+              width={35}
+              height={35}
+              alt="Jiran"
+            />
+            <h2 className="mt-10 text-2xl font-bold leading-9 tracking-tight text-center text-gray-900">
+              Sign in to your account
+            </h2>
+          </div>
+
+          <div className="space-y-6">
             <div>
-              <Image src={Logo} alt="logo" className="h-14 w-14" />
-              <h2 className="mt-6 text-3xl font-bold tracking-tight text-gray-900">
-                Sign in to your account
-              </h2>
+              <TextField
+                // @ts-ignore
+                id="email"
+                name="email"
+                label="email"
+                placeholder="email"
+                variant="outlined"
+                fullWidth
+              />
             </div>
 
-            <div className="mt-8">
-              <div>
-                <div>
-                  <p className="text-sm font-medium leading-6 text-gray-900">
-                    Sign in with
-                  </p>
+            <div>
+              <TextField
+                // @ts-ignore
+                id="password"
+                name="password"
+                label="password"
+                placeholder="password"
+                variant="outlined"
+                fullWidth
+              />
+            </div>
 
-                  <div className="grid grid-cols-3 gap-3 mt-2">
-                    <div>
-                      <a
-                        href="#"
-                        className="inline-flex justify-center w-full px-3 py-2 text-gray-500 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0"
-                      >
-                        <span className="sr-only">Sign in with Facebook</span>
-                        <svg
-                          className="w-5 h-5"
-                          aria-hidden="true"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M20 10c0-5.523-4.477-10-10-10S0 4.477 0 10c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V10h2.54V7.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V10h2.773l-.443 2.89h-2.33v6.988C16.343 19.128 20 14.991 20 10z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
-                      </a>
-                    </div>
-
-                    <div>
-                      <a
-                        href="#"
-                        className="inline-flex justify-center w-full px-3 py-2 text-gray-500 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0"
-                      >
-                        <span className="sr-only">Sign in with Twitter</span>
-                        <svg
-                          className="w-5 h-5"
-                          aria-hidden="true"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                        >
-                          <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                        </svg>
-                      </a>
-                    </div>
-
-                    <div>
-                      <a
-                        href="#"
-                        className="inline-flex justify-center w-full px-3 py-2 text-gray-500 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0"
-                      >
-                        <span className="sr-only">Sign in with GitHub</span>
-                        <svg
-                          className="w-5 h-5"
-                          aria-hidden="true"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="relative mt-6">
-                  <div
-                    className="absolute inset-0 flex items-center"
-                    aria-hidden="true"
-                  >
-                    <div className="w-full border-t border-gray-300" />
-                  </div>
-                  <div className="relative flex justify-center text-sm">
-                    <span className="px-2 text-gray-500 bg-white">
-                      Or continue with
-                    </span>
-                  </div>
-                </div>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <BaseInput
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  size="medium"
+                  className="mr-2 border-gray-300 rounded "
+                />
+                <Label label="remember me" />
               </div>
 
-              <div className="space-y-6">
-                <div>
-                  <TextField
-                    // @ts-ignore
-                    id="email"
-                    name="email"
-                    label="email"
-                    placeholder="email"
-                    variant="outlined"
-                    fullWidth
-                  />
-                </div>
-
-                <div>
-                  <TextField
-                    // @ts-ignore
-                    id="password"
-                    name="password"
-                    label="password"
-                    placeholder="password"
-                    variant="outlined"
-                    fullWidth
-                  />
-                </div>
-
-                <div className="flex items-center justify-between">
-                  <div className="flex items-center">
-                    <BaseInput
-                      id="remember-me"
-                      name="remember-me"
-                      type="checkbox"
-                      size="medium"
-                      className="mr-2 border-gray-300 rounded "
-                    />
-                    <Label label="remember me" />
-                  </div>
-
-                  <div className="text-sm">
-                    <a
-                      href="#"
-                      className="font-medium text-[indigo-600] hover:text-primary"
-                    >
-                      Forgot your password?
-                    </a>
-                  </div>
-                </div>
-
-                <div className="space-y-1">
-                  <div>
-                    <Button
-                      className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white rounded-md shadow-sm bg-primary hover:bg-white hover:text-primary hover:border-primary hover:border-2px"
-                      onClick={() => router.push("/main")}
-                    >
-                      Sign in
-                    </Button>
-                  </div>
-
-                  <div>
-                    <Button
-                      onClick={handleClick}
-                      className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white rounded-md shadow-sm bg-primary hover:bg-white hover:text-primary hover:border-primary hover:border-2px"
-                    >
-                      Voice Sign in
-                    </Button>
-                  </div>
-                </div>
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-medium text-[indigo-600] hover:text-primary"
+                >
+                  Forgot your password?
+                </a>
               </div>
+            </div>
+            <div>
+              <Button
+                className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white rounded-md shadow-sm bg-primary hover:bg-white hover:text-primary hover:border-primary hover:border-2px"
+                onClick={() => router.push("/main")}
+              >
+                Sign in
+              </Button>
             </div>
           </div>
-        </div>
-        <div className="relative flex-1 hidden w-0 lg:block">
-          <Image
-            className="absolute inset-0 object-cover w-full h-full"
-            src={photoFood}
-            alt="img"
-          />
         </div>
       </div>
     </>
